c900ae59bea0e6cc666f28a84bbe68d2769b16f7
Jason Little <little.129@osu.edu>
2014-09-06 20:48:09 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Needly::Application.config.secret_key_base = '931458615822c4093c6147a94faf90620ab91f7cd5cf767ce30236ad852937897f31f1948e711f78fcfdf12a88d635caf55474134f0482acb19f192f763c2135'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Needly::Application.config.session_store :cookie_store, key: '_needly_session'
